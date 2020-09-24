e9d545f47c61c9717a2bc9f9a386884213a15ba5
elna <elna.ruckerlypsa@gmail.com>
2020-09-24 14:09:08 +0200

    feat(Fragment): Include the fragment in the tablet mode

--- app/src/main/java/com/elnacabot/pac1/BookListActivity.java
+++ app/src/main/java/com/elnacabot/pac1/BookListActivity.java
@@ -1,10 +1,15 @@
 package com.elnacabot.pac1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -14,7 +19,7 @@ public class BookListActivity extends AppCompatActivity {
     private ListView list;
     private ArrayAdapter<String> adapter;
     private ArrayList<String> arrayList;
-
+    private FrameLayout frame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +28,7 @@ public class BookListActivity extends AppCompatActivity {
 
         list = findViewById(R.id.listElements);
 
-        String[] elements = new String[10];
+        final String[] elements = new String[10];
         for(int i = 0; i < 10; i++)
         {
             elements[i] = (i+1)+ "   Item " + (i+1);
@@ -34,6 +39,42 @@ public class BookListActivity extends AppCompatActivity {
         final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.book_detail, R.id.content, arrayList);
 
         list.setAdapter(arrayAdapter);
+
+        frame = findViewById(R.id.frame);
+        if(frame == null)
+        {
+            Toast.makeText(this, "TABLET", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            BookDetailFragment bookdetails = new BookDetailFragment();
+            getSupportFragmentManager().beginTransaction().add(R.id.frame, bookdetails).commit();
+        }
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                frame = findViewById(R.id.frame);
+                if(frame != null)
+                {
+                    /*TABLET*/
+                    BookDetailFragment detailFragment = new BookDetailFragment();
+                    Bundle bundle = new Bundle();
+                    bundle.putString("id", String.valueOf(i+1));
+                    detailFragment.setArguments(bundle);
+                    getSupportFragmentManager().beginTransaction().add(R.id.frame, detailFragment).commit();
+                }
+                else
+                {
+                    //Intent a BookDetailActivity
+                }
+
+            }
+        });
+
+
 
 
     }
