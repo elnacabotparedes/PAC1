86f0b317df11289432c24d899242cef2ed1d7a18
elna <elnacabot@uoc.edu>
2020-09-27 12:31:28 +0200

    feat(Customize): Customized the toolbar and the Bookdetail page

--- app/src/main/java/com/elnacabot/pac1/BookDetailActivity.java
+++ app/src/main/java/com/elnacabot/pac1/BookDetailActivity.java
@@ -1,16 +1,36 @@
 package com.elnacabot.pac1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 public class BookDetailActivity extends AppCompatActivity {
+
+    private androidx.appcompat.widget.Toolbar toolbar;
+    private TextView toolbarTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        //Hide Status bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_book_detail);
+
+        //Custom top bar
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
 
         Intent i = getIntent();
         String id = i.getStringExtra("id");
@@ -20,6 +40,9 @@ public class BookDetailActivity extends AppCompatActivity {
         BookDetailFragment detailFragment = new BookDetailFragment();
         detailFragment.setArguments(bundle);
         getSupportFragmentManager().beginTransaction().replace(R.id.frame, detailFragment).commit();
+
+        toolbarTitle = findViewById(R.id.toolbarTitle);
+        toolbarTitle.setText("Item "+id);
 
     }
 }
